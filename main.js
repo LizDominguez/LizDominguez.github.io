896d42baff404a8002ac4e805d6ea140b457e0e8
Liz Dominguez <liz.dominguez.leon@gmail.com>
2017-06-29 02:20:31 -0400

    removed smooth scroll & added snowflake

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
 /*jslint browser: true*/
 /*global $, jQuery*/
 
-jQuery(document).ready(function($){   
-  
-    $('a[href^="#"]').on('click', function(e) {
 
-        var target = $( $(this).attr('href') );
-
-        if( target.length ) {
-            e.preventDefault();
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 800);
-        }   
-    });  
-});
 
